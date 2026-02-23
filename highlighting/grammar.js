75b587b4d25d5720a0df86bb3d70799769a76c37
waterlens <waterlens@waterlens.moe>
2026-02-23 22:43:34 +0800

    refine highlight

--- highlighting/grammar.js
+++ highlighting/grammar.js
@@ -1,7 +1,7 @@
 const PREC = {
-  BINARY: 1,
-  UNARY: 2,
-  CALL: 3,
+  UNARY: 100,
+  CALL: 200,
+  OP_ID: 150,
 };
 
 module.exports = grammar({
@@ -13,37 +13,104 @@ module.exports = grammar({
   ],
 
   rules: {
-    source_file: $ => repeat($._expression_with_separator),
+    source_file: $ => optional($._block),
 
-    _expression_with_separator: $ => seq(
+    _block: $ => seq(
       $._expression,
-      optional(';')
+      repeat(seq(';', $._expression))
     ),
 
     _expression: $ => choice(
+      $.binary_expression,
+      $.unary_expression,
+      $.call_expression,
+      $.let_binding,
+      $.function_definition,
+      $.if_expression,
+      $.parenthesized_expression,
+      $.tuple_expression,
       $.identifier,
       $.number,
       $.string,
       $.boolean,
-      $.let_binding,
-      $.function_definition,
-      $.if_expression,
-      $.binary_expression,
-      $.unary_expression,
-      $.call_expression,
-      $.parenthesized_expression,
-      $.tuple_expression,
       $.operator_identifier,
+    ),
+
+    binary_expression: $ => choice(
+      prec.right(10, seq(field('left', $._expression), alias(':', $.operator), field('right', $._expression))),
+      prec.right(8, seq(field('left', $._expression), alias('@', $.operator), field('right', $._expression))),
+      prec.left(6, seq(field('left', $._expression), alias(choice('*', '/'), $.operator), field('right', $._expression))),
+      prec.left(4, seq(field('left', $._expression), alias(choice('+', '-'), $.operator), field('right', $._expression))),
+      prec.left(2, seq(field('left', $._expression), alias(choice('==', '!=', '<', '>', '<=', '>='), $.operator), field('right', $._expression))),
+      prec.left(1, seq(field('left', $._expression), $.operator, field('right', $._expression))),
+    ),
+
+    unary_expression: $ => prec(PREC.UNARY, seq($.operator, $._expression)),
+
+    call_expression: $ => prec.left(PREC.CALL, seq(
+      $._expression,
+      choice(
+        seq('(', sepBy(',', $._expression), ')'),
+        seq('[', sepBy(',', $._expression), ']'),
+        seq('{', sepBy(',', $._expression), '}'),
+      )
+    )),
+
+    parenthesized_expression: $ => seq(
+      '(',
+      $._expression,
+      ')'
+    ),
+
+    tuple_expression: $ => seq(
+      '(',
+      $._expression,
+      repeat1(seq(',', $._expression)),
+      ')'
+    ),
+
+    let_binding: $ => seq(
+      'let',
+      optional('rec'),
+      field('name', $.identifier),
+      '=',
+      field('value', $._expression)
+    ),
+
+    function_definition: $ => seq(
+      'fn',
+      field('parameters', $.parameters),
+      field('body', $._block),
+      'end'
+    ),
+
+    parameters: $ => seq(
+      '(',
+      sepBy(',', $.identifier),
+      ')'
+    ),
+
+    if_expression: $ => seq(
+      'if',
+      field('condition', $._expression),
+      'then',
+      field('consequence', $._expression),
+      'else',
+      field('alternative', $._expression),
+      'end'
     ),
 
     identifier: $ => /[a-zA-Z_][a-zA-Z0-9_!$%&*+\-/:<=>?@^~]*/,
 
     operator_identifier: $ => choice(
-      seq('(', $.operator, ')'),
-      seq('`', /[^`]*/, '`')
+      prec(PREC.OP_ID, seq('(', alias($.operator, $.operator), ')')),
+      prec(PREC.OP_ID, seq('`', alias(/[^`]*/, $.operator), '`')),
     ),
 
-    operator: $ => token(/[!$%&*+\-/:<=>?@^~|.#&?~]+/),
+    operator: $ => token(choice(
+      /[!$%&*+\-/:<=>?@^~|.#&?~]+/,
+      ',' , ';' , ':' , '.' , '?' , '~' , '!' , '$' , '&' , '*' , '+' , '-' , '/' , '=' , '>' , '@' , '^' , '|' , '%' , '<' , '#'
+    )),
 
     number: $ => {
       const hex = /0[xX][0-9a-fA-F]+/;
@@ -72,73 +139,12 @@ module.exports = grammar({
 
     boolean: $ => choice('true', 'false'),
 
-    let_binding: $ => seq(
-      'let',
-      optional('rec'),
-      field('name', $.identifier),
-      '=',
-      field('value', $._expression)
-    ),
-
-    function_definition: $ => seq(
-      'fn',
-      field('parameters', $.parameters),
-      field('body', repeat($._expression_with_separator)),
-      'end'
-    ),
-
-    parameters: $ => seq(
-      '(',
-      sepBy(',', $.identifier),
-      ')'
-    ),
-
-    if_expression: $ => seq(
-      'if',
-      field('condition', $._expression),
-      'then',
-      field('consequence', repeat($._expression_with_separator)),
-      'else',
-      field('alternative', repeat($._expression_with_separator)),
-      'end'
-    ),
-
-    binary_expression: $ => choice(
-      prec.left(PREC.BINARY, seq($._expression, $.operator, $._expression)),
-      prec.left(PREC.BINARY, seq($._expression, '@', $._expression)),
-      prec.left(PREC.BINARY, seq($._expression, ':', $._expression)),
-    ),
-
-    unary_expression: $ => prec(PREC.UNARY, seq($.operator, $._expression)),
-
-    call_expression: $ => prec.left(PREC.CALL, seq(
-      $._expression,
-      choice(
-        seq('(', sepBy(',', $._expression), ')'),
-        seq('[', sepBy(',', $._expression), ']'),
-        seq('{', sepBy(',', $._expression), '}'),
-      )
-    )),
-
-    parenthesized_expression: $ => seq(
-      '(',
-      $._expression,
-      ')'
-    ),
-
-    tuple_expression: $ => seq(
-      '(',
-      $._expression,
-      repeat1(seq(',', $._expression)),
-      ')'
-    ),
-
     comment: $ => seq(
       '(*',
       repeat(choice(
         $.comment,
-        /[^*]+/,
-        /\*[^)]/
+        /[^*]/,
+        seq('*', /[^)]/)
       )),
       '*)'
     ),
